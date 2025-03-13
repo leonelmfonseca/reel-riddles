6f2088cfdaa113d0227fb0d21189d56662c54e7e
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-13 14:19:32 +0000

    api: refine controller endpoints

--- src/main/java/com/leonelfonseca/reelriddles/reel_riddles/controller/ReelRiddlesController.java
+++ src/main/java/com/leonelfonseca/reelriddles/reel_riddles/controller/ReelRiddlesController.java
@@ -1,6 +1,10 @@
 package com.leonelfonseca.reelriddles.reel_riddles.controller;
 
 import com.leonelfonseca.reelriddles.reel_riddles.service.ReelRiddleService;
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.TmdbMovies;
+import info.movito.themoviedbapi.model.movies.MovieDb;
+import info.movito.themoviedbapi.tools.TmdbException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReelRiddlesController {
     
     private ReelRiddleService reelRiddleService;
-    
-    @Autowired
-    public ReelRiddlesController(ReelRiddleService reelRiddleService) {
+  private final TmdbApi tmdbApi;
+
+  @Autowired
+  public ReelRiddlesController(ReelRiddleService reelRiddleService, TmdbApi tmdbApi) {
         this.reelRiddleService = reelRiddleService;
+    this.tmdbApi = tmdbApi;
     }
-    
-    @GetMapping
-    public ResponseEntity<String> getRiddles() {
-        String riddle = reelRiddleService.executeFetch();
-        return ResponseEntity.ok(riddle);
+
+  @GetMapping("/tmdb")
+  public ResponseEntity<String> getTmdb() throws TmdbException {
+    TmdbMovies tmdbMovies = tmdbApi.getMovies();
+    MovieDb movie = tmdbMovies.getDetails(5353, "en-US");
+
+    return ResponseEntity.ok(movie.getTitle());
     }
-    
 }
